4d771e4e5f5229361cfd58dace2466448d331af3
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-02-04 16:02:10 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -4,14 +4,10 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Activate NVM in the current shell session
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+source ~/.bashrc
 
 # Install Node.js 19.5.0
 nvm install 19.5.0
-
-# Use Node.js 19.5.0 as default
-nvm alias default 19.5.0
 
 # Display Node.js and npm versions
 node -v
