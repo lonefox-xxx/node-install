9e0a6988d373b08d92be8899ff8b952a406f429a
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2024-04-29 12:46:29 +0530

    node version updated

--- node.sh
+++ node.sh
@@ -7,7 +7,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 
 # Install Node.js 19.5.0
-nvm install 19.5.0
+nvm install 20.11.1
 
 # Display Node.js and npm versions
 node -v
